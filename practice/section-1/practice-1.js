1ec9003587d32919680846740a7f5cbb398d30b1
GORDON YANG (ITA-ISDC-ISD-OOCLL/ZHA) <YANGGO@OOCL.COM>
2019-07-08 17:14:17 +0800

    update

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -2,8 +2,8 @@
 
 function collectSameElements(collectionA, collectionB) {
   var collection=new Array();
-  for(int i=0;i<collectionA.length;i++){
-      for(int j=0;j<collectionB.length;j++){
+  for(var i =0;i<collectionA.length;i++){
+      for(var j=0;j<collectionB.length;j++){
         if(collectionA[i]==collectionB[j]){
           collection.push(collectionA[i]);
         }
